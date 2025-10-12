b4c41e332d1d9174ce4a05dbf2b00baf5d5adf16
dbgbgtf <149954065+dbgbgtf1@users.noreply.github.com>
2025-10-12 13:01:17 +0800

    Update build_run.sh

--- docker/build_run.sh
+++ docker/build_run.sh
@@ -1,8 +1,6 @@
-NAME=$1
+docker build . -t
 
-docker build . -t ${NAME}
-
-docker run --name ${NAME} -v /home/dbgbgtf/attachment/:/mnt -p 3333:3333 -p 9999:9999  -it $NAME /bin/bash
+docker run --name -v /home/dbgbgtf/attachment/:/mnt -p 3333:3333 -p 9999:9999  -it container_name /bin/bash
 
 # container_id=$(docker ps -n 1 -q)
 # docker cp ${container_id}:/home/ctf/pwn .
